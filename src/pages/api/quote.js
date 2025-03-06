f466fc23ea5aeba8a0d5699566f564a21162c3d5
raquel <raquelmsmarques@gmail.com>
2025-03-06 12:55:01 +0000

    change api2

--- src/pages/api/quote.js
+++ src/pages/api/quote.js
@@ -2,24 +2,26 @@ import axios from 'axios';
 import pool from '../../lib/db';
 
 export default async function handler(req, res) {
+  let connection;
   try {
-    await pool.connect();
-    
     // Faz a requisição para a API externa
     const response = await axios.get('https://quotes-api-self.vercel.app/quote');
     
-     // Insere a citação no banco de dados
-     const query = 'INSERT INTO quote (quote, author) VALUES ($1, $2)';
-     const values = [response.data.quote, response.data.author];
-     await pool.query(query, values);
- 
-     // Retorna uma resposta de sucesso
-     res.status(200).json({ message: 'Quote inserted successfully' });
-     
-   } catch (error) {
-     console.error('Error fetching or inserting quote:', error);
-     res.status(500).json({ error: 'Failed to fetch or insert quote' });
-   } finally {
-     pool.end();
-   }
- }
+    // Obtém uma conexão do pool
+    connection = await pool.getConnection();
+    
+    // Insere a citação no banco de dados
+    const query = 'INSERT INTO quote (quote, author) VALUES (?, ?)';
+    const values = [response.data.quote, response.data.author];
+    await connection.query(query, values);
+
+    // Retorna uma resposta de sucesso
+    res.status(200).json({ message: 'Quote inserted successfully' });
+    
+  } catch (error) {
+    console.error('Error fetching or inserting quote:', error);
+    res.status(500).json({ error: 'Failed to fetch or insert quote' });
+  } finally {
+    if (connection) connection.release();
+  }
+}
